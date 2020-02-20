e9fa701f3dd6007fba193d5e48edc8077963d1f9
Jakob Molskness <jlmolskness@aol.com>
2020-02-19 23:00:49 -0500

    Test class for DBCredentialsProvider. Achieves 100% line coverage.

---
+++ Lambdas/test/DBCredentialsProviderTest.java
@@ -0,0 +1,68 @@
+import DoctorsNote.DBCredentialsProvider;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.io.IOException;
+
+public class DBCredentialsProviderTest {
+    private static DBCredentialsProvider dbcp;
+
+    @BeforeClass
+    public static void BeforeClass() {
+        try {
+            dbcp = new DBCredentialsProvider();
+        } catch (IOException e) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void getDBProvider() {
+        String actual = dbcp.getDBProvider();
+        Assert.assertNotNull(actual);
+        Assert.assertNotEquals("", actual);
+    }
+
+    @Test
+    public void getDBEndpoint() {
+        String actual = dbcp.getDBEndpoint();
+        Assert.assertNotNull(actual);
+        Assert.assertNotEquals("", actual);
+    }
+
+    @Test
+    public void getDBPort() {
+        String actual = dbcp.getDBPort();
+        Assert.assertNotNull(actual);
+        Assert.assertNotEquals("", actual);
+    }
+
+    @Test
+    public void getDBURL() {
+        String actual = dbcp.getDBURL();
+        Assert.assertNotNull(actual);
+        Assert.assertNotEquals("", actual);
+    }
+
+    @Test
+    public void getDBUsername() {
+        String actual = dbcp.getDBUsername();
+        Assert.assertNotNull(actual);
+        Assert.assertNotEquals("", actual);
+    }
+
+    @Test
+    public void getDBPassword() {
+        String actual = dbcp.getDBPassword();
+        Assert.assertNotNull(actual);
+        Assert.assertNotEquals("", actual);
+    }
+
+    @Test
+    public void getDBDriver() {
+        String actual = dbcp.getDBDriver();
+        Assert.assertNotNull(actual);
+        Assert.assertNotEquals("", actual);
+    }
+}
