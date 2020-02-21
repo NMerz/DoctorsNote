92c48786a90b0435da5aede0a98e568695e26eeb
Jakob Molskness <jlmolskness@aol.com>
2020-02-20 21:51:12 -0500

    Added test to verify connection object opens valid connection

--- Lambdas/test/DBCredentialsProviderTest.java
+++ Lambdas/test/DBCredentialsProviderTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.junit.Assert;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBCredentialsProviderTest {
     private static DBCredentialsProvider dbcp;
@@ -71,5 +74,21 @@ public class DBCredentialsProviderTest {
         String actual = dbcp.getDBDriver();
         Assert.assertNotNull(actual);
         Assert.assertNotEquals("", actual);
+    }
+
+    @Test
+    public void testConnection() {
+        try {
+            DBCredentialsProvider dbCP = new DBCredentialsProvider();
+            Class.forName(dbCP.getDBDriver());     // Loads and registers the driver
+            Connection connection = DriverManager.getConnection(dbCP.getDBURL(),
+                    dbCP.getDBUsername(),
+                    dbCP.getDBPassword());
+
+            Assert.assertTrue(connection.isValid(5));
+            connection.close();
+        } catch (IOException | SQLException | ClassNotFoundException e) {
+            Assert.fail();
+        }
     }
 }
