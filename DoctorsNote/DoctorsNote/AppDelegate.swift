bf8c2c789e07de31815e8029a37d186d28d4f0ec
NMerz <nmerz@icloud.com>
2020-02-25 15:37:10 -0500

    Correct array JSON
    Full unit tests for JSON conformation failure need work still

--- DoctorsNote/DoctorsNote/AppDelegate.swift
+++ DoctorsNote/DoctorsNote/AppDelegate.swift
@@ -16,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let connector = Connector()
-        let connectionProcessor = ConnectionProcessor(connector: connector)
-        connectionProcessor.retrieveData(urlString: ConnectionProcessor.standardUrl())
+        //let connectionProcessor = ConnectionProcessor(connector: connector)
+        //connectionProcessor.retrieveData(urlString: ConnectionProcessor.standardUrl())
         return true
     }
 
