137746e565c451ed5d6ff92f7d72dc70d5b44315
smit3019 <smit3019>
2020-02-28 17:44:16 -0500

    Added Doctor Tables and new model

--- Database/Total SQL.sql
+++ Database/Total SQL.sql
@@ -18,89 +18,80 @@ CREATE SCHEMA IF NOT EXISTS `DoctorsNote` DEFAULT CHARACTER SET utf8 COLLATE utf
 USE `DoctorsNote` ;
 
 -- -----------------------------------------------------
+-- Table `DoctorsNote`.`Address`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DoctorsNote`.`Address` (
+  `addressID` INT(11) NOT NULL,
+  `Street1` VARCHAR(45) NULL DEFAULT NULL,
+  `Street2` VARCHAR(45) NULL DEFAULT NULL,
+  `City` VARCHAR(45) NULL DEFAULT NULL,
+  `State` VARCHAR(2) NULL DEFAULT NULL COMMENT 'State code of two letters e.g. OH\\n\\nMight need to change this for foreign countries.',
+  `Country` VARCHAR(45) NULL DEFAULT NULL,
+  `PostalCode` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`addressID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+
+-- -----------------------------------------------------
 -- Table `DoctorsNote`.`Conversation`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `DoctorsNote`.`Conversation` (
   `conversationID` INT(11) NOT NULL AUTO_INCREMENT,
   `conversationName` VARCHAR(45) NULL DEFAULT NULL,
-  `lastMessageTime` DATETIME NULL DEFAULT NULL,
+  `lastMessageTime` DATETIME(3) NULL DEFAULT NULL,
   `status` TINYINT(4) NULL DEFAULT NULL,
   PRIMARY KEY (`conversationID`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 17
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
 
-
--- -----------------------------------------------------
--- Table `DoctorsNote`.`Message`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `DoctorsNote`.`Message` (
-  `messageID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `content` LONGTEXT NULL DEFAULT NULL,
-  `sender` INT(10) UNSIGNED NOT NULL,
-  `recipient` INT(10) UNSIGNED NOT NULL,
-  `timeCreated` DATETIME NULL DEFAULT NULL,
-  `conversationID` INT(11) NOT NULL,
-  PRIMARY KEY (`messageID`, `conversationID`),
-  CONSTRAINT `fk_Message_Conversation1`
-    FOREIGN KEY (`conversationID`)
-    REFERENCES `DoctorsNote`.`Conversation` (`conversationID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_bin;
-
--- -----------------------------------------------------
--- Table `DoctorsNote`.`Address`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `DoctorsNote`.`Address` (
-  `addressID` INT NOT NULL,
-  `Street1` VARCHAR(45) NULL,
-  `Street2` VARCHAR(45) NULL,
-  `City` VARCHAR(45) NULL,
-  `State` VARCHAR(2) NULL COMMENT 'State code of two letters e.g. OH\n\nMight need to change this for foreign countries.',
-  `Country` VARCHAR(45) NULL,
-  `PostalCode` INT NULL,
-  PRIMARY KEY (`addressID`))
-ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `DoctorsNote`.`Personal Information`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `DoctorsNote`.`Personal Information` (
-  `personalInformationID` INT NOT NULL,
+  `personalInformationID` INT(11) NOT NULL,
   `Name` VARCHAR(45) NOT NULL,
-  `Birth Date` DATE NULL,
-  `Sex` VARCHAR(10) NULL,
-  `Phone Number` INT(11) NULL,
-  `addressID` INT NOT NULL,
+  `Birth Date` DATE NULL DEFAULT NULL,
+  `Sex` VARCHAR(10) NULL DEFAULT NULL,
+  `Phone Number` INT(11) NULL DEFAULT NULL,
+  `addressID` INT(11) NOT NULL,
   PRIMARY KEY (`personalInformationID`, `addressID`),
   CONSTRAINT `fk_Personal Information_Address1`
     FOREIGN KEY (`addressID`)
     REFERENCES `DoctorsNote`.`Address` (`addressID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+
 
 -- -----------------------------------------------------
 -- Table `DoctorsNote`.`Patient`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `DoctorsNote`.`Patient` (
-  `patientID` INT NOT NULL,
-  `Nickname` VARCHAR(45) NULL,
-  `Public Key` INT NOT NULL,
-  `Failed Logins` INT NULL,
-  `Username` VARCHAR(45) NULL,
-  `personalInformationID` INT NOT NULL,
-  `addressID` INT NOT NULL,
-  PRIMARY KEY (`patientID`, `personalInformationID`, `addressID`),
-  CONSTRAINT `fk_Patient_Personal Information1`
-    FOREIGN KEY (`personalInformationID` , `addressID`)
-    REFERENCES `DoctorsNote`.`Personal Information` (`personalInformationID` , `addressID`)
+  `patientID` INT(11) NOT NULL,
+  `Nickname` VARCHAR(45) NULL DEFAULT NULL,
+  `Public Key` INT(11) NOT NULL,
+  `Failed Logins` INT(11) NULL DEFAULT NULL,
+  `Username` VARCHAR(45) NULL DEFAULT NULL,
+  `personalInformationID` INT(11) NOT NULL,
+  PRIMARY KEY (`patientID`, `personalInformationID`),
+  CONSTRAINT `fk_Patient_PersonalInformation1`
+    FOREIGN KEY (`personalInformationID`)
+    REFERENCES `DoctorsNote`.`Personal Information` (`personalInformationID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+
 
 -- -----------------------------------------------------
 -- Table `DoctorsNote`.`Conversation_has_User`
@@ -122,6 +113,69 @@ CREATE TABLE IF NOT EXISTS `DoctorsNote`.`Conversation_has_User` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
+
+
+
+-- -----------------------------------------------------
+-- Table `DoctorsNote`.`Doctor`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DoctorsNote`.`Doctor` (
+  `doctorID` INT(11) NOT NULL AUTO_INCREMENT,
+  `systemID` INT(11) NULL DEFAULT NULL,
+  `Name` VARCHAR(45) NULL DEFAULT NULL,
+  `Location` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`doctorID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 987654322
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+
+-- -----------------------------------------------------
+-- Table `DoctorsNote`.`Doctor_has_Patient`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DoctorsNote`.`Doctor_has_Patient` (
+  `doctorID` INT(11) NOT NULL,
+  `patientID` INT(11) NOT NULL,
+  PRIMARY KEY (`doctorID`, `patientID`),
+  CONSTRAINT `fk_Doctor_has_Patient_Doctor1`
+    FOREIGN KEY (`doctorID`)
+    REFERENCES `DoctorsNote`.`Doctor` (`doctorID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Doctor_has_Patient_Patient1`
+    FOREIGN KEY (`patientID`)
+    REFERENCES `DoctorsNote`.`Patient` (`patientID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+
+
+-- -----------------------------------------------------
+-- Table `DoctorsNote`.`Message`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DoctorsNote`.`Message` (
+  `messageID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `content` LONGTEXT NULL DEFAULT NULL,
+  `sender` INT(10) UNSIGNED NOT NULL,
+  `recipient` INT(10) UNSIGNED NOT NULL,
+  `timeCreated` DATETIME NULL DEFAULT CURRENT_TIMESTAMP(),
+  `conversationID` INT(11) NOT NULL,
+  PRIMARY KEY (`messageID`, `conversationID`),
+  CONSTRAINT `fk_Message_Conversation1`
+    FOREIGN KEY (`conversationID`)
+    REFERENCES `DoctorsNote`.`Conversation` (`conversationID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 11
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
