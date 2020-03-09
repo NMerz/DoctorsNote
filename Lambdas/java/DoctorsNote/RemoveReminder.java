2e9a8d40a707987ecb82937a33e710beba575b51
Jakob Molskness <jlmolskness@aol.com>
2020-03-08 21:19:26 -0400

    Wrapper for removing reminders

---
+++ Lambdas/java/DoctorsNote/RemoveReminder.java
@@ -0,0 +1,32 @@
+package DoctorsNote;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.Map;
+
+/*
+ * A Lambda handler for removing reminders from a user.
+ *
+ * Expects: A JSON body with the following fields: reminderID (String)
+ * Returns: TBD
+ *
+ * Error Handling: Returns null if an unrecoverable error is encountered
+ */
+
+public class RemoveReminder implements RequestHandler<Map<String,Object>, ReminderRemover.RemoveReminderResponse> {
+    @Override
+    public ReminderRemover.RemoveReminderResponse handleRequest(Map<String,Object> inputMap, Context context) {
+        // Establish connection with MariaDB
+        ReminderRemover remover = new ReminderRemover(Connector.getConnection());
+        ReminderRemover.RemoveReminderResponse response = remover.remove(inputMap, context);
+        if (response == null) {
+            throw new RuntimeException("Server experienced an error");
+        }
+        return response;
+    }
+
+    public static void main(String[] args) throws IllegalStateException {
+        throw new IllegalStateException();
+    }
+}
