2650f01874d6b01655001b674b43f7e8936d9c84
bbhardin <bbhardin1@gmail.com>
2020-03-13 20:48:04 -0400

    Begins work on view for reporting an issue.

--- DoctorsNote/DoctorsNote/ProfileViewController.swift
+++ DoctorsNote/DoctorsNote/ProfileViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PopupKit
 import AWSCognito
 import AWSMobileClient
 
@@ -14,6 +15,9 @@ class ProfileViewController: UIViewController {
    
     @IBOutlet weak var logOutButton: UIButton!
     @IBOutlet weak var personalInfoView: PersonalInfoView!
+    @IBOutlet weak var settingsButton: UIButton!
+    
+    var p: PopupView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,6 +50,64 @@ class ProfileViewController: UIViewController {
         
         AWSMobileClient.default().signOut()
     
+    }
+    
+    @IBAction func showSettings(_ sender: Any) {
+    
+        let width : Int = Int(self.view.frame.width - 20)
+        let height = 500
+
+        let contentView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: width, height: height))
+        contentView.backgroundColor = UIColor.white
+        let maskLayer = CAShapeLayer()
+        maskLayer.path = UIBezierPath(roundedRect: contentView.bounds, cornerRadius: 38.5).cgPath
+        contentView.layer.mask = maskLayer
+
+        p = PopupView.init(contentView: contentView)
+        p?.maskType = .dimmed
+
+        let nameLabel = UILabel(frame: CGRect(x: 20, y: 20, width: width - 40, height: 100))
+        nameLabel.text = "Group Name\n\nGroup Description"
+        nameLabel.numberOfLines = 5
+        
+        let descriptionOffset = Int(nameLabel.frame.height) + 40
+        let descriptionLabel = UILabel(frame: CGRect(x: 20, y: descriptionOffset, width: width - 20, height: 200))
+        
+        let messageOffset = 60 + Int(nameLabel.frame.height) + Int(descriptionLabel.frame.height)
+        let messageLabel = UILabel(frame: CGRect(x: 20, y: messageOffset, width: width - 40, height: 25))
+        nameLabel.text = "## Members"
+    
+        let closeButton = UIButton(frame: CGRect(x: width/2 - 45, y: height - 75, width: 90, height: 40))
+        closeButton.setTitle("Done", for: .normal)
+        closeButton.backgroundColor = UIColor.systemBlue
+        let layer = CAShapeLayer()
+        layer.path = UIBezierPath(roundedRect: closeButton.bounds, cornerRadius: DefinedValues.fieldRadius).cgPath
+        closeButton.layer.mask = layer
+        closeButton.addTarget(self, action: #selector(dismissPopup), for: .touchUpInside)
+
+        
+        let joinButton = UIButton(frame: CGRect(x: width/2 - 45, y: height - 75, width: 90, height: 40))
+        let joinLayer = CAShapeLayer()
+        joinLayer.path = UIBezierPath(roundedRect: joinButton.bounds, cornerRadius: DefinedValues.fieldRadius).cgPath
+        joinButton.layer.mask = joinLayer
+
+        contentView.addSubview(joinButton)
+        contentView.addSubview(closeButton)
+        contentView.addSubview(nameLabel)
+
+        let xPos = self.view.frame.width / 2
+        let yPos = self.view.frame.height / 2
+        let location = CGPoint.init(x: xPos, y: yPos)
+        p?.showType = .slideInFromBottom
+        p?.maskType = .dimmed
+        p?.dismissType = .slideOutToBottom
+        p?.show(at: location, in: (self.tabBarController?.view)!)
+        
+    }
+    
+    @objc func dismissPopup(sender: UIButton!) {
+        p?.dismissType = .slideOutToBottom
+        p?.dismiss(animated: true)
     }
     
 }
