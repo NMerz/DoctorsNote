12792601f3055753570f1c82595a1bc5af10f8e8
Jakob Molskness <jlmolskness@aol.com>
2020-03-22 18:06:21 -0400

    Unit test for RemoveReminder

---
+++ Lambdas/test/RemoveReminderTest.java
@@ -0,0 +1,48 @@
+import DoctorsNote.RemoveReminder;
+import DoctorsNote.ReminderRemover;
+import com.amazonaws.services.lambda.runtime.Context;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.HashMap;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+public class RemoveReminderTest {
+    Context contextMock;
+    RemoveReminder removeReminder;
+    ReminderRemover reminderRemoverMock;
+
+    @Before
+    public void beforeTests() {
+        contextMock = Mockito.mock(Context.class);
+        removeReminder = spy(new RemoveReminder());
+        reminderRemoverMock = Mockito.mock(ReminderRemover.class);
+        doReturn(reminderRemoverMock).when(removeReminder).makeReminderRemover();
+    }
+
+    @Test
+    public void testValidReturn() {
+        ReminderRemover.RemoveReminderResponse responseMock = Mockito.mock(ReminderRemover.RemoveReminderResponse.class);
+        when(reminderRemoverMock.remove(Mockito.anyMap(), Mockito.any())).thenReturn(responseMock);
+        HashMap<String, Object> inputMap = new HashMap<String, Object>();
+        Assert.assertEquals(responseMock, removeReminder.handleRequest(inputMap, contextMock));
+    }
+
+    @Test
+    public void testInvalidReturn() {
+        when(reminderRemoverMock.remove(Mockito.anyMap(), Mockito.any())).thenReturn(null);
+        HashMap<String, Object> inputMap = new HashMap<String, Object>();
+
+        try {
+            removeReminder.handleRequest(inputMap, contextMock);
+            Assert.fail();
+        } catch (RuntimeException e) {
+            Assert.assertEquals("Server experienced an error", e.getMessage());
+        }
+    }
+}
