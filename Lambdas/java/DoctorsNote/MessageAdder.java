71cbe49075ce22e19a1efb52d594f2b9eeafa696
Jakob Molskness <jlmolskness@aol.com>
2020-04-10 15:57:10 -0500

    Update to conversationlist to fix empty return bug

--- Lambdas/java/DoctorsNote/MessageAdder.java
+++ Lambdas/java/DoctorsNote/MessageAdder.java
@@ -3,6 +3,7 @@ package DoctorsNote;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.google.gson.Gson;
 
+import javax.jnlp.UnavailableServiceException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.time.Instant;
@@ -21,6 +22,8 @@ import java.util.Map;
  */
 public class MessageAdder {
     private final String addMessageFormatString = "INSERT INTO Message (content, sender, timeCreated, conversationID, contentType) VALUES (?, ?, ?, ?, ?);";
+    private final String incrementMessagesSentString = "UPDATE Metrics SET value = value + 1 WHERE name = 'messagesSent';";
+    private final String incrementMessagesFailedString = "UPDATE Metrics SET value = value + ? WHERE name = 'messagesFailed';";
     Connection dbConnection;
 
     public MessageAdder(Connection dbConnection) { this.dbConnection = dbConnection; }
@@ -47,8 +50,27 @@ public class MessageAdder {
 
             if (ret == 0) {
                 System.out.println("MessageAdder: Update successful");
+
+                System.out.println("MessageAdder: Incrementing metric messagesSent by 1");
+                PreparedStatement messagesSentStatement = dbConnection.prepareStatement(incrementMessagesSentString);
+                messagesSentStatement.executeUpdate();
+
+                int nFailures;
+
+                try {
+                    nFailures = Integer.parseInt((String)((Map<String,Object>) inputMap.get("body-json")).get("numFails"));
+                } catch (Exception e) {
+                    nFailures = 0;
+                }
+
+                System.out.println("MessageAdder: Incrementing metric messagesFailed by " + nFailures);
+
+                PreparedStatement messagesFailureStatement = dbConnection.prepareStatement(incrementMessagesFailedString);
+                messagesFailureStatement.setInt(1, nFailures);
+                messagesFailureStatement.executeUpdate();
             } else {
                 System.out.println(String.format("MessageAdder: Update failed (%d)", ret));
+                throw new UnavailableServiceException("Unable to update database");
             }
 
             // Disconnect connection with shortest lifespan possible
